404ee9f556a812301b7a773da53f298b2c3c816f
Washingmachineli <1105351108@qq.com>
2021-03-04 01:11:09 +0800

    2021/3/4

--- blogServer/app.js
+++ blogServer/app.js
@@ -41,6 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json({limit:'50mb'}));
 app.use(bodyParser.urlencoded({limit:'50mb',extended:true}));
+app.use(express.json({limit: '50mb'}))
 
 
 app.use('/', indexRouter);
